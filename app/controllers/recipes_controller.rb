d038fa293eda6700ad770da406cb6f8be86da55b
Kazim110 <kazim.mohammadi110@gmail.com>
2024-02-07 22:54:13 +0430

    add cancancan and public recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,36 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def public_recipes
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc).includes(:user)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def toggle_visibility
+    if @recipe.user == current_user
+      @recipe.update(public: !@recipe.public)
+      redirect_to @recipe, notice: 'Visibility updated successfully.'
+    else
+      redirect_to @recipe, alert: 'You are not authorized to perform this action.'
+    end
+  end
+
+  def destroy
+    if @recipe.user == current_user
+      @recipe.destroy
+      redirect_to recipes_url, notice: 'Recipe was successfully destroyed.'
+    else
+      redirect_to @recipe, alert: 'You are not authorized to perform this action.'
+    end
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+end
