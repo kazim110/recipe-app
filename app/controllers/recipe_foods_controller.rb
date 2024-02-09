d89012b3b5d4b9d0d099a7b13421c0a54d17d73e
Kazim110 <kazim.mohammadi110@gmail.com>
2024-02-10 02:18:27 +0430

    fix linter error

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -5,9 +5,5 @@ class RecipeFoodsController < ApplicationController
     @recipe_foods = current_user.recipe_foods.includes(:food, :recipe)
     @total_items = @recipe_foods.sum(:quantity)
     @total_price = @recipe_foods.sum { |item| item.quantity * item.food.price }
-
-    if @recipe_foods.empty?
-      flash.now[:alert] = "Your shopping list is empty."
-    end
   end
 end
