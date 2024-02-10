c7e64c008a6fb88295807ec283139d5ef2d2c209
MARK RUBANZA <markrubanza10@gmail.com>
2024-02-11 01:05:11 +0300

    fix linter errors

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -22,37 +22,37 @@ RSpec.describe Recipe, type: :model do
     )
     it 'is valid with valid attributes' do
       recipe = Recipe.new(
-        recipe:,
+        recipe:
       )
       expect(recipe).to be_valid
     end
 
-    it "is not valid without a name" do
+    it 'is not valid without a name' do
       expect(recipe).not_to be_valid
       expect(recipe.errors[:name]).to include("can't be blank")
     end
 
-    it "is not valid without a user" do
+    it 'is not valid without a user' do
       expect(recipe).not_to be_valid
-      expect(recipe.errors[:user]).to include("must exist")
+      expect(recipe.errors[:user]).to include('must exist')
     end
 
-    it "is not valid with a negative preparation time" do
+    it 'is not valid with a negative preparation time' do
       expect(recipe).not_to be_valid
-      expect(recipe.errors[:preparation_time]).to include("must be greater than or equal to 0")
+      expect(recipe.errors[:preparation_time]).to include('must be greater than or equal to 0')
     end
 
-    it "is not valid with a negative cooking time" do
+    it 'is not valid with a negative cooking time' do
       expect(recipe).not_to be_valid
-      expect(recipe.errors[:cooking_time]).to include("must be greater than or equal to 0")
+      expect(recipe.errors[:cooking_time]).to include('must be greater than or equal to 0')
     end
 
-    it "belongs to a user" do
+    it 'belongs to a user' do
       association = described_class.reflect_on_association(:user)
       expect(association.macro).to eq :belongs_to
     end
-    
-    it "has many recipe_foods" do
+
+    it 'has many recipe_foods' do
       association = described_class.reflect_on_association(:recipe_foods)
       expect(association.macro).to eq :has_many
     end
