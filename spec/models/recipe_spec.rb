fa4576b60a6acbc673205e19408d566603cbcba8
MARK RUBANZA <markrubanza10@gmail.com>
2024-02-11 01:21:25 +0300

    Correct Rubocop errors

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,57 +5,55 @@ RSpec.describe Recipe, type: :model do
   let(:recipe) do
     Recipe.new(
       name: 'Apple Cake',
-      user:,
+      user: user,
       preparation_time: 15,
       cooking_time: 15,
-      discription: 'very delicious recipe!',
+      description: 'very delicious recipe!',
       public: true
     )
   end
   let(:food) do
     Food.new(
       name: 'Orange',
-      user:,
+      user: user,
       measurement_unit: 'gram',
       price: 15,
       quantity: 500
     )
-    it 'is valid with valid attributes' do
-      recipe = Recipe.new(
-        recipe:
-      )
-      expect(recipe).to be_valid
-    end
-
-    it 'is not valid without a name' do
-      expect(recipe).not_to be_valid
-      expect(recipe.errors[:name]).to include("can't be blank")
-    end
-
-    it 'is not valid without a user' do
-      expect(recipe).not_to be_valid
-      expect(recipe.errors[:user]).to include('must exist')
-    end
-
-    it 'is not valid with a negative preparation time' do
-      expect(recipe).not_to be_valid
-      expect(recipe.errors[:preparation_time]).to include('must be greater than or equal to 0')
-    end
-
-    it 'is not valid with a negative cooking time' do
-      expect(recipe).not_to be_valid
-      expect(recipe.errors[:cooking_time]).to include('must be greater than or equal to 0')
-    end
-
-    it 'belongs to a user' do
-      association = described_class.reflect_on_association(:user)
-      expect(association.macro).to eq :belongs_to
-    end
-
-    it 'has many recipe_foods' do
-      association = described_class.reflect_on_association(:recipe_foods)
-      expect(association.macro).to eq :has_many
-    end
-    # Add more association tests as needed
   end
+
+  it 'is valid with valid attributes' do
+    expect(recipe).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    expect(recipe).not_to be_valid
+    expect(recipe.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is not valid without a user' do
+    expect(recipe).not_to be_valid
+    expect(recipe.errors[:user]).to include('must exist')
+  end
+
+  it 'is not valid with a negative preparation time' do
+    expect(recipe).not_to be_valid
+    expect(recipe.errors[:preparation_time]).to include('must be greater than or equal to 0')
+  end
+
+  it 'is not valid with a negative cooking time' do
+    expect(recipe).not_to be_valid
+    expect(recipe.errors[:cooking_time]).to include('must be greater than or equal to 0')
+  end
+
+  it 'belongs to a user' do
+    association = described_class.reflect_on_association(:user)
+    expect(association.macro).to eq :belongs_to
+  end
+
+  it 'has many recipe_foods' do
+    association = described_class.reflect_on_association(:recipe_foods)
+    expect(association.macro).to eq :has_many
+  end
+  # Add more association tests as needed
 end
